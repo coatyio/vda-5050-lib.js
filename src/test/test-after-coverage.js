420fccaf03858630266d98c479c6451000e2b50e
ffa500 <hubertus.hohl@siemens.com>
2021-03-03 21:35:02 +0100

    chore: release test coverage report on docs/coverage

--- src/test/test-after-coverage.js
+++ src/test/test-after-coverage.js
@@ -1,26 +1,33 @@
 /*! Copyright (c) 2021 Siemens AG. Licensed under the MIT License. */
 
-const fs = require("fs");
+const fse = require("fs-extra");
 const lcovTotal = require("lcov-total");
+const path = require("path");
 
-function coverageSummary() {
+function releaseCoverageReport() {
+    const docsFolder = "docs/coverage/";
+
     try {
+        fse.emptyDirSync(docsFolder);
+        fse.copySync("coverage", docsFolder);
+
         const coveragePercent = Math.floor(lcovTotal("./coverage/lcov.info"));
 
         // Create a shields.io JSON endpoint for a dynamic coverage badge in the
         // project's README.md.
-        const shieldsioJsonFile = ".coverage.shieldsio.json";
-        fs.writeFileSync(shieldsioJsonFile, JSON.stringify({
+        const shieldsioJsonFile = path.join(docsFolder, ".coverage.shieldsio.json");
+        fse.writeFileSync(shieldsioJsonFile, JSON.stringify({
             "schemaVersion": 1,
             "label": "coverage",
             "message": `${coveragePercent}%`,
             "color": "brightgreen"
         }));
 
+        console.log("Released coverage report in", docsFolder);
         console.log("Created coverage badge info with %d% in %s", coveragePercent, shieldsioJsonFile);
     } catch (error) {
-        console.error("Coverage badge info could not be created: %s", error);
+        console.error("Coverage report and badge couldn't be released to %s: %s", docsFolder, error);
     }
 }
 
-coverageSummary();
+releaseCoverageReport();
